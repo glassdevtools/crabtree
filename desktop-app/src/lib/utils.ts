f6709bcc776aed2dfa7347ad33380b65e3b4dbc3
Andrew Pareles <andrewpareles@MacBook-Pro.local>
2026-05-01 02:58:01 -0700

    t

--- desktop-app/src/lib/utils.ts
+++ desktop-app/src/lib/utils.ts
@@ -3,5 +3,6 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export const cn = (...inputs: ClassValue[]) => {
+  // hello world
   return twMerge(clsx(inputs));
 };
