6bdbf6df75ddfb8adc030714198719e2bdcd60b5
Andrew Pareles <andrewpareles@gmail.com>
2026-05-05 15:52:11 -0700

    merge

--- website/app/twitter-image.tsx
+++ website/app/twitter-image.tsx
@@ -5,20 +5,9 @@ import {
   socialImageSize,
 } from "./social-image";
 
-const twitterImageVersion = "v2";
-
 export const alt = socialImageAlt;
 export const contentType = socialImageContentType;
 export const runtime = "nodejs";
 export const size = socialImageSize;
-
-export const generateImageMetadata = () => [
-  {
-    id: twitterImageVersion,
-    alt: socialImageAlt,
-    contentType: socialImageContentType,
-    size: socialImageSize,
-  },
-];
 
 export default createSocialImage;
